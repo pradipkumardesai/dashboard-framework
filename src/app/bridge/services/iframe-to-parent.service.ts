aaea632868813df70abc5b05e1a738cc9f0ef336
pradipkumardesai <pradipkumarsemail@gmail.com>
2018-06-22 11:31:15 +0530

    basic bridge service

--- src/app/bridge/services/iframe-to-parent.service.ts
+++ src/app/bridge/services/iframe-to-parent.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class IframeToParentService {
 
-  constructor() { }
+  onParentMessageReceived = new Subject<string>();
 
+  constructor() { 
+    window.addEventListener("message", this.receiveMessage.bind(this), false);
+  }
+
+  public sendMessageToParent(message){
+    window.parent.postMessage(message, '*');
+  }
+
+  private receiveMessage(event){
+    if (event.origin === "http://localhost:4200")
+      this.onParentMessageReceived.next(event.data);
+  }
 }
--- src/app/bridge/services/parent-to-iframe.service.ts
+++ src/app/bridge/services/parent-to-iframe.service.ts
@@ -3,9 +3,8 @@ import { Subject } from "rxjs";
 
 @Injectable()
 export class ParentToIframeService {
-
   private onFromIframeMessage = new Subject<string>();
-  private that=this;
+  private that = this;
 
   constructor() {
     console.log(this);
@@ -16,14 +15,12 @@ export class ParentToIframeService {
     iframeElement.contentWindow.postMessage(JSON.stringify(messageObject), "*");
   }
 
-  receiveMessage(event){
+  receiveMessage(event) {
     if (event.origin === "http://localhost:4300")
-    this.getOnFromIframeMessageRef().next(JSON.stringify(event.data));
-     
+      this.getOnFromIframeMessageRef().next(JSON.stringify(event.data));
   }
 
-  getOnFromIframeMessageRef(){
+  getOnFromIframeMessageRef() {
     return this.onFromIframeMessage;
   }
-
 }
